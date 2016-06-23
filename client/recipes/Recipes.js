1247ed9e673699658297e5a769199457d68e9797
DusanMinic <dushanminic@gmail.com>
2016-06-23 12:47:35 +0200

    Added template level subscriptions

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,7 +1,11 @@
-Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function () {
+	this.autorun(() => {
+		this.subscribe('recipes');
+	});
+});
 
 Template.Recipes.helpers({
 	recipes: () => {
-		return Recipes.find({});
+		return Recipes.find({}, { sort: { createdAt: -1 } });
 	}
 });
