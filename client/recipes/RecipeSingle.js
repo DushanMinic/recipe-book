3458e5a6b6f7ba871c6a8a75864d399de59f14a0
DusanMinic <dushanminic@gmail.com>
2016-06-23 13:36:39 +0200

    Displaying Single Recipe now

---
+++ client/recipes/RecipeSingle.js
@@ -0,0 +1,13 @@
+Template.RecipeSingle.onCreated(function () {
+	this.autorun(() => {
+		const id = FlowRouter.getParam('id');
+		this.subscribe('singleRecipe', id);
+	});
+});
+
+Template.RecipeSingle.helpers({
+	recipe: () => {
+		const id = FlowRouter.getParam('id');
+		return Recipes.findOne({_id: id});
+	}
+});
--- server/publish.js
+++ server/publish.js
@@ -1,6 +1,8 @@
-// import { Mongo } from 'meteor/mongo';
-// import { Meteor } from 'meteor/meteor';
-
 Meteor.publish('recipes', function() {
 	return Recipes.find({ author: this.userId });
+});
+
+Meteor.publish('singleRecipe', function(id) {
+	check(id, String);
+	return Recipes.find({ _id: id });
 });
