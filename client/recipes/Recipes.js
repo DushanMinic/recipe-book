389a4485653c7b6025743007b38a4d0c7523fac3
DusanMinic <dushanminic@gmail.com>
2016-06-24 17:51:17 +0200

    Toggle on/off New Recipe form

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -7,5 +7,11 @@ Template.Recipes.onCreated(function () {
 Template.Recipes.helpers({
 	recipes: () => {
 		return Recipes.find({});
-	}
+	},
+});
+
+Template.Recipes.events({
+	'click .new-recipe' () {
+		Session.set('newRecipe', true);
+	},
 });
